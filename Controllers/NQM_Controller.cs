4a96a43977e1619953ef1af9a36e2d410c006500
NGUYỄN QUNAG MẠNH <146909381+nicenot@users.noreply.github.com>
2024-04-22 22:34:06 +0700

    123

---
+++ Controllers/NQM_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace manh.Controllers
+{
+    public class NQM_Controller : Controller
+    {
+        // GET: NQM_
+        public ActionResult Index()
+        {
+            ViewData["msg"] = "dữ liệu trong view data";
+            return View();
+        }
+
+        public ActionResult StudentList()
+        {
+            string[] names = { "manh manh", "ho bao", "soc nho" };
+            ViewBag.Names = names;
+            return View();
+        }
+        public ActionResult Insert() { 
+            return View("Addstudent");
+        }
+    }
+}
